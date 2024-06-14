04d732e387ac22666fe51d1608e8ecfc93b69e35
kavya_kunder <kavyakunder7082@gmail.com>
2024-06-15 00:54:56 +0530

    initial commit2

---
+++ frontend/src/App.js
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+
+function App() {
+  const [aiResponse, setAiResponse] = useState("");
+  const [questionCount, setQuestionCount] = useState(0);
+  const startListening = () => {
+    SpeechRecognition.startListening({
+      continuous: true,
+      language: "en-IN",
+      interimResults: true,
+    });
+  };
+
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
+  console.log("listening", listening);
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+
+  function speak(aiResponse) {
+    console.log("speak", transcript);
+    // Create a SpeechSynthesisUtterance
+    const utterance = new SpeechSynthesisUtterance(aiResponse);
+
+    // Select a voice
+    const voices = speechSynthesis.getVoices();
+    utterance.voice = voices[1]; // Choose a specific voice
+    utterance.lang = "en-IN";
+    // Speak the text
+    speechSynthesis.speak(utterance);
+  }
+  const stopListening = async () => {
+    SpeechRecognition.stopListening();
+
+    try {
+      const response = await axios.post("/api/chat", { transcript });
+      console.log("ressss", response);
+      setAiResponse(response.data.content);
+      speak(response.data.content);
+      setQuestionCount(questionCount + 1);
+      resetTranscript();
+    } catch (error) {
+      console.error("Error during conversation:", error);
+    }
+  };
+
+  console.log("question count is", questionCount);
+
+  return (
+    <div>
+      <p>Microphone: {listening ? "on" : "off"}</p>
+      <button
+        // onClick={() =>
+        //   SpeechRecognition.startListening({
+        //     continuous: true,
+        //     interimResults: true,
+        //   })
+        // }
+        onClick={startListening}
+      >
+        Start
+      </button>
+
+      {/* <button onClick={SpeechRecognition.startListening}>Start</button> */}
+      <button onClick={stopListening}>Stop</button>
+      {/*<button onClick={SpeechRecognition.stopListening}>Stop</button>*/}
+
+      {/* <button onClick={resetTranscript}>Reset</button> */}
+      <p>User: {transcript}</p>
+      {/* </form> */}
+      {/* <button onClick={speak}>AI voice</button> */}
+      <p>AI Response: {aiResponse}</p>
+    </div>
+  );
+}
+
+export default App;
