16aa2c8b910dec2bacb4beebfaf98703cd5c27f6
kavya_kunder <kavyakunder7082@gmail.com>
2024-06-16 00:26:17 +0530

    feat: add footer

--- frontend/src/pages/RootLayout.jsx
+++ frontend/src/pages/RootLayout.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/ui/Header";
 import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
 
 export default function RootLayout() {
   return (
     <>
       <Header />
       <Outlet />
+      <Footer />
     </>
   );
 }
