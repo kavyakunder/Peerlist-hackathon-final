21c7e39ff34e676deb91fd2d809c09b81f633ea1
MadhushreeKunder <madhushreekunder@gmail.com>
2024-06-15 12:56:20 +0530

    add styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,7 @@
 import LandingPage from "./pages/LandingPage";
 
 function App() {
-  return (
-    <div>
-      <LandingPage />
-    </div>
-  );
+  return <LandingPage />;
 }
 
 export default App;
--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,5 +1,20 @@
-import React from "react";
+import landingPageStyles from "../styles/landingPage.module.css";
 
 export default function LandingPage() {
-  return <div>LandingPage</div>;
+  return (
+    <main className={`${landingPageStyles.app}`}>
+      <div className={`${landingPageStyles.container}`}>
+        <div className={`${landingPageStyles.content}`}>
+          <h1 className={`${landingPageStyles.heroText}`}>
+            Ace Your Interviews with AI Precision
+          </h1>
+          <p className={`${landingPageStyles.description}`}>
+            QnAce is your AI-powered interview coach. Experience realistic mock
+            interviews, get personalized feedback, and master every question.
+          </p>
+          <button className={`${landingPageStyles.startButton}`}>Start</button>
+        </div>
+      </div>
+    </main>
+  );
 }
