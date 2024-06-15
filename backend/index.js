7c136fbd65dccaa9b33702f3507e70f104fa82ca
kavya_kunder <kavyakunder7082@gmail.com>
2024-06-15 11:10:51 +0530

    feat: handle cors error

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const app = express();
 const chatRoute = require("./server.js");
+const cors = require("cors");
 
-// Middleware
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Add this block
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 
-// API routes
 app.use("/api", chatRoute);
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-// Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
