8b10de7ffc897815f421ec400807fe02a5c5a159
MadhushreeKunder <madhushreekunder@gmail.com>
2024-06-17 01:16:30 +0530

    make responsive

--- frontend/src/constants/categories.js
+++ frontend/src/constants/categories.js
@@ -1,11 +1,11 @@
 const ALL_LEVELS = [
-  { id: 1, name: "Easy" },
-  { id: 2, name: "Medium" },
-  { id: 3, name: "Hard" },
+  { id: 1, name: "Beginner" },
+  { id: 2, name: "Intermediate" },
+  { id: 3, name: "Advanced" },
 ];
 const ALL_TOPICS = [
   { id: 1, name: "React" },
-  { id: 2, name: "Javascript" },
+  { id: 2, name: "JavaScript" },
   { id: 3, name: "CSS" },
 ];
 export { ALL_LEVELS, ALL_TOPICS };
