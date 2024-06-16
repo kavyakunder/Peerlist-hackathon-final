bbc76edceade324808a76fd82d878cf9d83b08b0
MadhushreeKunder <madhushreekunder@gmail.com>
2024-06-16 16:29:32 +0530

    final feedbakc page

--- frontend/src/pages/FeedbackPage.jsx
+++ frontend/src/pages/FeedbackPage.jsx
@@ -2,40 +2,58 @@ import { Doughnut } from "react-chartjs-2";
 import StarRating from "../components/ui/StarRating";
 import feedbackPageStyles from "../styles/feedbackPage.module.css";
 import { data, options } from "../components/PieChart";
+import { Link } from "react-router-dom";
+import landingPageStyles from "../styles/landingPage.module.css";
 
-const feedback = [
-  {
-    id: 1,
-    response: `For the first question, you should have answered "Redux" instead of "used react use State".`,
-    rating: 3,
-  },
-  {
-    id: 2,
-    response: `For the second question, you got it right! You correctly answered that a smaller reusable UI component is called a "component".`,
-    rating: 5,
-  },
-  {
-    id: 3,
-    response: `For the third question, you listed multiple libraries, but didn't quite get the one I was looking for. You should have answered "Tailwind CSS" instead of listing multiple libraries.`,
-    rating: 2,
-  },
-];
+const interviewFeedback = {
+  technicalFeedback: [
+    {
+      id: 1,
+      response: `For the first question, you should have answered "Redux" instead of "used react use State".`,
+      rating: 3,
+    },
+    {
+      id: 2,
+      response: `For the second question, you got it right! You correctly answered that a smaller reusable UI component is called a "component".`,
+      rating: 5,
+    },
+    {
+      id: 3,
+      response: `For the third question, you listed multiple libraries, but didn't quite get the one I was looking for. You should have answered "Tailwind CSS" instead of listing multiple libraries.`,
+      rating: 2,
+    },
+  ],
+  generalFeedback: `You didn't quite get the answers right, but you did show some enthusiasm and engagement throughout the conversation. Keep practicing and learning, and you'll get better with time!`,
+};
 
 export default function FeedbackPage() {
   return (
     <div className={feedbackPageStyles.feedbackPageLayout}>
-      <h1> Technical Feedback </h1>
-      {feedback.map((item) => (
-        <div key={item.id} className={feedbackPageStyles.feedback}>
-          <p>{item.id}.</p>
-          <p className={feedbackPageStyles.feedbackText}>{item.response}</p>
-          <StarRating rating={item.rating} />
-        </div>
-      ))}
-      <h1> Analysis </h1>
-      <div>
-        <Doughnut data={data} width={500} height={500} options={options} />
+      <div className={feedbackPageStyles.feedbackSection}>
+        <h1>Technical Feedback</h1>
+        {interviewFeedback.technicalFeedback.map((item) => (
+          <div key={item.id} className={feedbackPageStyles.feedbackContent}>
+            <div className={feedbackPageStyles.feedback}>
+              <p>{item.id}.</p>
+              <p className={feedbackPageStyles.feedbackText}>{item.response}</p>
+            </div>
+            <StarRating rating={item.rating} />
+          </div>
+        ))}
       </div>
+      <div className={feedbackPageStyles.feedbackSection}>
+        <h1>Analysis</h1>
+        <div>
+          <Doughnut data={data} width={500} height={500} options={options} />
+        </div>
+      </div>
+      <div className={feedbackPageStyles.feedbackSection}>
+        <h1>General Feedback</h1>
+        <p>{interviewFeedback.generalFeedback}</p>
+      </div>
+      <Link to="/" className={landingPageStyles.startButton}>
+        Go to home page
+      </Link>
     </div>
   );
 }
