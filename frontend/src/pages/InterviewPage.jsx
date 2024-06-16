ec343b068621324e34b7dd602b726ca442000849
kavya_kunder <kavyakunder7082@gmail.com>
2024-06-16 11:02:36 +0530

    feat: integrate backend with frontend

--- frontend/src/pages/InterviewPage.jsx
+++ frontend/src/pages/InterviewPage.jsx
@@ -1,8 +1,61 @@
+import { useState } from "react";
 import interviewStyles from "../styles/interviewPage.module.css";
 import qnAceInterviewer from "../assets/qnAceInterviewer.jpg";
 import qnAceInterviewee from "../assets/qnAceInterviewee.png";
+import axios from "axios";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 
 export default function InterviewPage() {
+  const [aiResponse, setAiResponse] = useState("");
+
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition</span>;
+  }
+
+  const handleStartListening = () => {
+    SpeechRecognition.startListening({
+      continuous: true,
+      language: "en-IN",
+      interimResults: true,
+    });
+  };
+
+  const handleStopListening = async () => {
+    SpeechRecognition.stopListening();
+
+    try {
+      const response = await axios.post(
+        "https://peerlist-hackathon-final.vercel.app/api/chat",
+        {
+          transcript,
+        }
+      );
+      setAiResponse(response.data.content);
+      speak(response.data.content);
+      resetTranscript();
+    } catch (error) {
+      console.error("Error during conversation:", error);
+    }
+  };
+
+  function speak(aiResponse) {
+    const utterance = new SpeechSynthesisUtterance(aiResponse);
+
+    const voices = speechSynthesis.getVoices();
+    utterance.voice = voices[1];
+    utterance.lang = "en-IN";
+    speechSynthesis.speak(utterance);
+  }
+
   return (
     <div className={interviewStyles.interviewLayout}>
       <div className={interviewStyles.interviewLHS}>
@@ -26,14 +79,19 @@ export default function InterviewPage() {
             className={interviewStyles.intervieweeProfile}
           />
           <div className={interviewStyles.intervieweeControlButtonGroup}>
-            <button className={interviewStyles.intervieweeControlButton}>
+            <button
+              onClick={handleStartListening}
+              className={interviewStyles.intervieweeControlButton}
+            >
               Start recording
             </button>
             <button
+              onClick={handleStopListening}
               className={interviewStyles.intervieweeControlButtonInactive}
             >
               Stop recording
             </button>
+            <p>Microphone: {listening ? "on" : "off"}</p>
           </div>
         </div>
       </div>
@@ -43,10 +101,11 @@ export default function InterviewPage() {
         >
           <p className={interviewStyles.sectionTitle}>Question</p>
           <p className={interviewStyles.sectionContent}>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
+            {/* Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
             delectus cum quis exercitationem, earum unde vero a facilis pariatur
             inventore molestias nostrum consectetur dicta sit magni possimus
-            amet minima quod!
+            amet minima quod! */}
+            {aiResponse}
           </p>
         </div>
         <div
@@ -54,7 +113,7 @@ export default function InterviewPage() {
         >
           <p className={interviewStyles.sectionTitle}>Answer</p>
           <p className={interviewStyles.sectionContent}>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
+            {/* Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
             delectus cum quis exercitationem, earum unde vero a facilis pariatur
             inventore molestias nostrum consectetur dicta sit magni possimus
             amet minima quod! Lorem ipsum dolor sit amet, consectetur
@@ -64,7 +123,8 @@ export default function InterviewPage() {
             amet, consectetur adipisicing elit. Asperiores delectus cum quis
             exercitationem, earum unde vero a facilis pariatur inventore
             molestias nostrum consectetur dicta sit magni possimus amet minima
-            quod!
+            quod! */}
+            {transcript}
           </p>
         </div>
       </div>
