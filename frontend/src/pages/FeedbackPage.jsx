3a9b40ddecbf8cdd43bea2fc87119b64e00b21a1
MadhushreeKunder <madhushreekunder@gmail.com>
2024-06-17 13:54:15 +0530

    add loader

--- frontend/src/pages/FeedbackPage.jsx
+++ frontend/src/pages/FeedbackPage.jsx
@@ -6,11 +6,13 @@ import { useEffect, useState } from "react";
 import { DEV_URL, LOCAL_URL } from "../api";
 import axios from "axios";
 import { checkPattern, interviewFeedbackExpectedFormat } from "../util";
+import Loader from "./Loader";
 
 export default function FeedbackPage() {
   const [interviewFeedback, setInterviewFeedback] = useState({});
   const [fallBackMsg, setFallBackMsg] = useState("");
   const [isFeedbackValid, setIsFeedbackValid] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     handleGetFeedback();
@@ -33,73 +35,88 @@ export default function FeedbackPage() {
             const response = await axios.get(`${LOCAL_URL}/api/feedback-error`);
             if (response?.data?.message) {
               const fallbackResponse = response?.data?.message;
+              setIsLoading(false);
               setFallBackMsg(fallbackResponse);
             }
           } catch (err) {
+            setIsLoading(false);
             setFallBackMsg("Apologies! Feedback cannot be generated.");
           }
         } else {
+          setIsLoading(false);
           setInterviewFeedback(parsedFeedback);
         }
       }
     } catch (error) {
+      setIsLoading(false);
       setFallBackMsg("Apologies! Feedback cannot be generated.");
     }
   };
 
   return (
-    <div className={feedbackPageStyles.feedbackPageLayout}>
-      {isFeedbackValid ? (
-        <>
-          <div className={feedbackPageStyles.feedbackSection}>
-            <h1 className={feedbackPageStyles.centeredTitle}>
-              Technical Feedback
-            </h1>
-            {interviewFeedback?.technicalFeedback?.map((item) => (
-              <div key={item.id} className={feedbackPageStyles.feedbackContent}>
-                <p className={feedbackPageStyles.feedbackText}>
-                  {item.feedback}
-                </p>
-                <StarRating rating={item.rating.toString().split("/")[0]} />
-              </div>
-            ))}
-          </div>
-          <div className={feedbackPageStyles.feedbackSection}>
-            <h1 className={feedbackPageStyles.centeredTitle}>Analysis</h1>
-
-            {interviewFeedback?.analysis?.map((item) => {
-              return (
-                <div
-                  key={item.id}
-                  className={feedbackPageStyles.progressBarContainer}
-                >
-                  <p>{item.paramName}</p>
-                  <div className={feedbackPageStyles.progress}>
-                    <div
-                      style={{ width: `${item.percent}%` }}
-                      className={feedbackPageStyles.progressValue}
-                    />
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-          <div className={feedbackPageStyles.feedbackSection}>
-            <h1 className={feedbackPageStyles.centeredTitle}>
-              General Feedback
-            </h1>
-            <p>{interviewFeedback?.generalFeedback}</p>
-          </div>
-        </>
+    <>
+      {isLoading ? (
+        <Loader text="QnAce is generating customized feedback for you" />
       ) : (
-        <div className={feedbackPageStyles.feedbackSection}>
-          <h1 className={feedbackPageStyles.centeredTitle}>Overall Feedback</h1>
-          <p>{fallBackMsg}</p>
+        <div className={feedbackPageStyles.feedbackPageLayout}>
+          {isFeedbackValid ? (
+            <>
+              <div className={feedbackPageStyles.feedbackSection}>
+                <h1 className={feedbackPageStyles.centeredTitle}>
+                  Technical Feedback
+                </h1>
+                {interviewFeedback?.technicalFeedback?.map((item) => (
+                  <div
+                    key={item.id}
+                    className={feedbackPageStyles.feedbackContent}
+                  >
+                    <p className={feedbackPageStyles.feedbackText}>
+                      {item.feedback}
+                    </p>
+                    <StarRating rating={item.rating.toString().split("/")[0]} />
+                  </div>
+                ))}
+              </div>
+              <div className={feedbackPageStyles.feedbackSection}>
+                <h1 className={feedbackPageStyles.centeredTitle}>Analysis</h1>
+
+                {interviewFeedback?.analysis?.map((item) => {
+                  return (
+                    <div
+                      key={item.id}
+                      className={feedbackPageStyles.progressBarContainer}
+                    >
+                      <p>{item.paramName}</p>
+                      <div className={feedbackPageStyles.progress}>
+                        <div
+                          style={{ width: `${item.percent}%` }}
+                          className={feedbackPageStyles.progressValue}
+                        />
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+              <div className={feedbackPageStyles.feedbackSection}>
+                <h1 className={feedbackPageStyles.centeredTitle}>
+                  General Feedback
+                </h1>
+                <p>{interviewFeedback?.generalFeedback}</p>
+              </div>
+            </>
+          ) : (
+            <div className={feedbackPageStyles.feedbackSection}>
+              <h1 className={feedbackPageStyles.centeredTitle}>
+                Overall Feedback
+              </h1>
+              <p>{fallBackMsg}</p>
+            </div>
+          )}
+          <Link to="/" className={landingPageStyles.startButton}>
+            Go to home page
+          </Link>
         </div>
       )}
-      <Link to="/" className={landingPageStyles.startButton}>
-        Go to home page
-      </Link>
-    </div>
+    </>
   );
 }
